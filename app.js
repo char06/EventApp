aa485822eaaf2987b288c6e2f277fa780db26f35
char06 <carlosaguirre086@gmail.com>
2023-03-15 16:23:07 -0700

    Connecting DB

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-
+//This code was added to create the server with express
 const express = require('express'); 
+const path = require('path');
+const mongoose = require('mongoose'); 
+//const methodOverride = require('method-override');
+const Event = require('./models/event');
+
+//This is code to connect the dababase with our app. The host number was suggested by mongodb documentation 
+mongoose.connect('mongodb://localhost:27017/event-app', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}); 
+
+//This code was created to confirm the db is connnected if not, throw an error. 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
 const app = express();
 
-// add comments about what the above is 
+//This code will connect the views or pages of the app with our express server. 
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views'))
+
+//This code was added to confirm our server is connected with our app. 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
+
---
+++ models/event.js
@@ -0,0 +1,16 @@
+//This code was added to connect and add the mongo database to our event app
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//An Object Data Modeling (ODM) library for MongoDB and Node.js is called Mongoose. It handles data associations, offers schema validation, and translates between objects created in code and how MongoDB represents those same items.
+
+//The document's structure, default values, validators, etc. are all specified in a Mongoose schema.
+
+const EventSchema  = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+module.exports = mongoose.model('Event', EventSchema); 
